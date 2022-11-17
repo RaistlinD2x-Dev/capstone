3ac93d71590c3f0c5cf5f4456fbcb212fb9f1ebe
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2022-11-17 14:47:08 -0600

    added api gateway added first helloworld lambda with proxy integration figured out separate lambda and apigateway stacks/constructs is broken

--- bin/final-project.ts
+++ bin/final-project.ts
@@ -6,17 +6,20 @@ import { FrontendStack } from '../lib/ui/frontend-stack';
 import { DataStoreStack } from '../lib/data-store/data-store-stack';
 import { DataIngestionStack } from '../lib/data-ingestion/data-ingestion-stack';
 import { ForecastStack } from '../lib/forecast/forecast-stack';
+import { ApiStack } from '../lib/api/api-stack';
 
 const app = new cdk.App();
 
-const network = new NetworkStack(app, 'NetworkStack');
+// const network = new NetworkStack(app, 'NetworkStack');
 
-const frontend = new FrontendStack(app, 'Frontend', {
-  vpc: network.vpc,
-});
+// const frontend = new FrontendStack(app, 'Frontend', {
+//   vpc: network.vpc,
+// });
 
 const datastore = new DataStoreStack(app, 'Datastore', {});
 
 const dataIngestion = new DataIngestionStack(app, 'DataIngestion', {});
 
 const forecast = new ForecastStack(app, 'Forecast', {});
+
+const api = new ApiStack(app, 'API', {});
---
+++ lib/api/lambdas/helloworld/index.py
@@ -0,0 +1,10 @@
+def handler(event, context):
+
+  response = {
+    "statusCode": 200,
+    "body": "Hello World!"
+  }
+  
+  return response
+
+
