3268b6241bc0ab044fa80a26b08b96a938de330c
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2022-11-23 03:59:35 -0600

    fixed get-stocks lambda proxy response issues

--- src/lambdas/get-stocks/index.py
+++ src/lambdas/get-stocks/index.py
@@ -5,19 +5,23 @@ from constants import *
 from ssm import *
 
 
-def handler(event,context):
+def handler(event, context):
 
     url = API_URL
 
     API_KEY = get_parameter_value(STOCK_API_PARAM_NAME)
 
-    querystring = {"exchange":"NASDAQ","format":"json"}
+    querystring = {"exchange": "NASDAQ", "format": "json"}
 
     headers = {
         "X-RapidAPI-Key": API_KEY,
-        "X-RapidAPI-Host": "twelve-data1.p.rapidapi.com"
+        "X-RapidAPI-Host": "twelve-data1.p.rapidapi.com",
     }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
 
-    return response.json()
+    jsonResponse = response.json()
+
+    result = {"statusCode": 200, "body": json.dumps(jsonResponse)}
+
+    return result
